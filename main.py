59c77651766b28cfc691ac2d8880d30e7cceb1c3
Joseph K B <kbjoseph2@gmail.com>
2023-06-08 10:38:22 +0530

    Inital code with main.py as the working app

---
+++ main.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+
+
+# Load the image
+image = cv2.imread('images/an.jpeg')
+
+
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply thresholding to obtain binary image
+ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+# Find contours in the binary image
+contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Initialize a list to store similar object pairs
+similar_pairs = []
+
+# Iterate over contours to compare object similarity
+for i in range(len(contours)):
+    for j in range(i+1, len(contours)):
+        # Extract the bounding rectangles for the current pair of objects
+        rect1 = cv2.boundingRect(contours[i])
+        rect2 = cv2.boundingRect(contours[j])
+
+        # Calculate the similarity score based on the bounding rectangle area ratio
+        similarity = cv2.matchShapes(contours[i], contours[j], cv2.CONTOURS_MATCH_I2, 0)
+
+        # Define a threshold for determining similar objects
+        similarity_threshold = 0.1
+
+        # If similarity score is below the threshold, consider the objects similar
+        if similarity < similarity_threshold:
+            similar_pairs.append((rect1, rect2))
+
+# Draw bounding boxes and label similar object pairs
+for pair in similar_pairs:
+    rect1, rect2 = pair
+    x1, y1, w1, h1 = rect1
+    x2, y2, w2, h2 = rect2
+
+    # Draw the bounding rectangles for similar objects
+    cv2.rectangle(image, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+    cv2.rectangle(image, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), 2)
+
+# Display the labeled image
+cv2.imshow("Labeled Image",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
